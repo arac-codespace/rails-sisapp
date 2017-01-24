9adc046458add92a2a91d4103bc0f39ec399dc2b
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-01-24 19:20:13 +0000

    Added drama index styling.  Preparing to implement search/cort capabilities.

--- sisblog/app/controllers/dramas_controller.rb
+++ sisblog/app/controllers/dramas_controller.rb
@@ -7,7 +7,9 @@ class DramasController < ApplicationController
 
   # GET /drama_index  
   def index
+    
     @dramas = Drama.all
+    # @dramas = @dramas_array.sort_by{|s| s[:title]}
     @category = params[:cat]
   end
   
