cb5e12e75b8d6ce2799285ba869cc1565d19e41e
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-01-28 05:47:28 +0000

    Implemented disqus comment system. Customized public_activity log for site's major sections.

--- sisblog/app/controllers/pages_controller.rb
+++ sisblog/app/controllers/pages_controller.rb
@@ -9,8 +9,12 @@ class PagesController < ApplicationController
   
   def resenas
     # public_activity collects tracked information here.
-    # The information to be tracked is determined in the model controller file!
+    # The information to be tracked is determined in the new form file!
     @activities = PublicActivity::Activity.order('created_at desc').limit(10)
+    # Here I take the public_activity hash, order it and put a condition on it.
+    # The idea is to limit the tracked activity to the pertaining web section.
+    @activities_condition = "Drama", "Other", "Movie", "Book","Makeup","FacialProduct","Anime"
+    @activities_by_ctrl = @activities.where({ trackable_type: [@activities_condition] })
 
     add_breadcrumb "Inicio", :root_path  
     add_breadcrumb "Reseñas", :resenas_path
@@ -19,14 +23,19 @@ class PagesController < ApplicationController
   def cuidado_personal
     
     @activities = PublicActivity::Activity.order('created_at desc').limit(10)
+    @activities_condition = "Care"
+    @activities_by_ctrl = @activities.where({ trackable_type: [@activities_condition] })    
     
     add_breadcrumb "Inicio", :root_path  
     add_breadcrumb "Cuidado Personal", :cuidado_personal_path
   end
   
   def recomendaciones
-    @activities = PublicActivity::Activity.order('created_at desc').limit(10)
     
+    @activities = PublicActivity::Activity.order('created_at desc').limit(10)
+    @activities_condition = "Recommendation"
+   
+    @activities_by_ctrl = @activities.where({ trackable_type: [@activities_condition] })        
     add_breadcrumb "Inicio", :root_path  
     add_breadcrumb "Recomendaciones", :recomendaciones_path
   end
