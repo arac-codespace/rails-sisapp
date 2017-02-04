8f5ca358e1a06e0ca6bc7481c304fefd42e252a3
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-02-04 07:32:38 +0000

    Modified routes to authenticate user with admin role to gain access to certain routes.

--- sisblog/config/routes.rb
+++ sisblog/config/routes.rb
@@ -14,64 +14,64 @@ Rails.application.routes.draw do
   
   
   #Only an authenticated user can get access to these actions
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :dramas, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :dramas, only: [:index, :show]
 
   #Only an authenticated user can get access to these actions 
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :movies, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :movies, only: [:index, :show]
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :animes, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :animes, only: [:index, :show]
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :books, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :books, only: [:index, :show]
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :makeups, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :makeups, only: [:index, :show]  
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :facial_products, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :facial_products, only: [:index, :show]    
   
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :others, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :others, only: [:index, :show]   
   
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
     resources :crafts, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :crafts, only: [:index, :show] 
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
   resources :cares, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :cares, only: [:index, :show]   
   
-  authenticate :user do
+  authenticate :user, lambda {|u| u.admin?} do
   resources :recommendations, only: [:new, :create, :edit, :update, :destroy]
   end
   
