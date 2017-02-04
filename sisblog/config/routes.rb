a581eecebc9876c259b0bdbf8fab3904b2e0c0b3
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-02-04 07:24:00 +0000

    Fixed button if statements for user admin role.

--- sisblog/config/routes.rb
+++ sisblog/config/routes.rb
@@ -14,64 +14,64 @@ Rails.application.routes.draw do
   
   
   #Only an authenticated user can get access to these actions
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :dramas, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :dramas, only: [:index, :show]
 
   #Only an authenticated user can get access to these actions 
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :movies, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :movies, only: [:index, :show]
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :animes, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :animes, only: [:index, :show]
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :books, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :books, only: [:index, :show]
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :makeups, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :makeups, only: [:index, :show]  
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :facial_products, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :facial_products, only: [:index, :show]    
   
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :others, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :others, only: [:index, :show]   
   
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
     resources :crafts, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :crafts, only: [:index, :show] 
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
   resources :cares, only: [:new, :create, :edit, :update, :destroy]
   end
   
   resources :cares, only: [:index, :show]   
   
-  authenticate :user, lambda {|u| u.admin?} do
+  authenticate :user do
   resources :recommendations, only: [:new, :create, :edit, :update, :destroy]
   end
   
