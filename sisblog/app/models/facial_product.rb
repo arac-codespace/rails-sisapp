3910a9095e137afdec58d5f5933b68e6ad079241
Alex Aguirre <alex.aguirre0026@gmail.com>
2017-05-18 05:21:21 +0000

    Lowered generated id number and added model title to URL

--- sisblog/app/models/facial_product.rb
+++ sisblog/app/models/facial_product.rb
@@ -8,10 +8,14 @@ class FacialProduct < ActiveRecord::Base
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/  
   
+  def to_param
+    "#{id} #{title}".parameterize
+  end
+  
   private
   def randomize_id
     begin
-      self.id = SecureRandom.random_number(1_000_000)
+      self.id = SecureRandom.random_number(9_999)
     end while FacialProduct.where(id: self.id).exists?
   end  
 end
--- sisblog/app/models/movie.rb
+++ sisblog/app/models/movie.rb
@@ -9,12 +9,15 @@ class Movie < ActiveRecord::Base
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/  
   
+  def to_param
+    "#{id} #{title}".parameterize
+  end
   
   # Generates an unique id for the object to use for disqus unique id 
   private
   def randomize_id
     begin
-      self.id = SecureRandom.random_number(1_000_000)
+      self.id = SecureRandom.random_number(9_999)
     end while Movie.where(id: self.id).exists?
   end
   
